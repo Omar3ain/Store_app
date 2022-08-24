2cb2c4f54b9913fcb67a86d069968d5bf4dbf545
Omar3ain <94538121+Omar3ain@users.noreply.github.com>
2022-08-24 15:42:26 +0200

    done with product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component'
+import { ProductItemDetailsComponent } from './components/product-item-details/product-item-details.component'
 
 const routes: Routes = [
-  {path: '' , component: ProductListComponent}
+  {path: '' , component: ProductListComponent},
+  {path:'product-item-detials/:id' , component:ProductItemDetailsComponent}
 ];
 
 @NgModule({
--- src/app/components/product-item-details/product-item-details.component.ts
+++ src/app/components/product-item-details/product-item-details.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GetDataService } from 'src/app/services/get-data.service';
+import { storeModel } from 'src/app/models/storeDataModel';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-product-item-details',
   templateUrl: './product-item-details.component.html',
   styleUrls: ['./product-item-details.component.css']
 })
 export class ProductItemDetailsComponent implements OnInit {
-
-  constructor() { }
+  id : number = 5;
+  product : storeModel = {
+    id: 0,
+    name: '',
+    price:0,
+    url: '',
+    description: ''
+  }
+  constructor(private getdata :GetDataService ,private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
+    this.id = +this.route.snapshot.paramMap.get('id')!
+    this.getdata.getData().subscribe({
+      next:(res) => {
+        this.product = res[this.id-1] 
+      }
+    })
   }
 
 }
